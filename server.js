60b02be86534fb4282230fd9b4038f423e0a6e56
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-04-30 16:40:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,19 +5,16 @@ const io = require('socket.io')(http);
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-
 const rooms = {};
 
-// ГЛАВНОЕ ИЗМЕНЕНИЕ: Указываем, что статика лежит в папке 'public'
 app.use(express.static(path.join(__dirname, 'public')));
-
-// Если кто-то заходит на главную, отдаем файл из папки public
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
 io.on('connection', (socket) => {
     socket.on('join_room', (data) => {
+        // Простая проверка пароля (можешь сменить '1234' на свой)
+        if(data.pass !== '1234') return socket.emit('auth_error', 'НЕВЕРНЫЙ ПАРОЛЬ');
+        
         socket.join(data.room);
         socket.room = data.room;
         socket.userName = data.name;
@@ -44,16 +41,16 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('chat_msg', (msg) => {
-        if (socket.room) io.to(socket.room).emit('receive_msg', msg);
-    });
-
     socket.on('clear', () => {
         if (socket.room) {
             rooms[socket.room].objects = [];
             io.to(socket.room).emit('clear_all');
         }
     });
+
+    socket.on('chat_msg', (msg) => {
+        if (socket.room) io.to(socket.room).emit('receive_msg', msg);
+    });
 });
 
-http.listen(PORT, () => console.log(`SCORPION SERVER ACTIVE ON PORT ${PORT}`));
+http.listen(PORT, () => console.log(`SCORPION V17 ACTIVE`));
