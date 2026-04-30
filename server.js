6d03cbbbf9ad595ff98be89bbef878347ce06787
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-04-30 07:51:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,78 +1,46 @@
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-const users = {};
-const rooms = {}; 
-const roomMarkers = {}; // Хранилище меток для каждой комнаты
+// ... (подключение socket.io)
+let users = {};
+let items = {};
 
 io.on('connection', (socket) => {
     socket.on('join_room', (data) => {
-        const { roomID, callsign, password } = data;
-
-        if (!rooms[roomID]) {
-            rooms[roomID] = password;
-            roomMarkers[roomID] = []; // Создаем пустой массив меток для новой комнаты
-        } else if (rooms[roomID] !== password) {
-            return socket.emit('error_msg', 'НЕВЕРНЫЙ КОД ДОСТУПА');
-        }
-        
-        socket.join(roomID);
-        users[socket.id] = { id: socket.id, roomID, callsign, lat: null, lng: null };
-        
+        socket.join(data.room);
+        users[socket.id] = data;
         socket.emit('login_success');
-        // Отправляем новому игроку все существующие метки в этой комнате
-        socket.emit('init_markers', roomMarkers[roomID]);
     });
 
-    socket.on('update_gps', (coords) => {
-        const user = users[socket.id];
-        if (user) {
-            user.lat = coords.lat;
-            user.lng = coords.lng;
-            io.to(user.roomID).emit('presence_update', getRoomUsers(user.roomID));
+    socket.on('gps_sync', (pos) => {
+        if(users[socket.id]) {
+            users[socket.id].lat = pos.lat;
+            users[socket.id].lng = pos.lng;
+            io.to(users[socket.id].room).emit('users_sync', users);
         }
     });
 
-    // Обработка новой метки
-    socket.on('new_map_marker', (markerData) => {
-        const user = users[socket.id];
-        if (user && roomMarkers[user.roomID]) {
-            roomMarkers[user.roomID].push(markerData);
-            io.to(user.roomID).emit('draw_marker', markerData);
+    socket.on('new_obj', (m) => {
+        if(users[socket.id]) {
+            if(!items[users[socket.id].room]) items[users[socket.id].room] = [];
+            items[users[socket.id].room].push(m);
+            io.to(users[socket.id].room).emit('draw', m);
         }
     });
 
-    // Очистка всех меток
-    socket.on('clear_all_markers', () => {
-        const user = users[socket.id];
-        if (user) {
-            roomMarkers[user.roomID] = [];
-            io.to(user.roomID).emit('markers_cleared');
+    socket.on('msg_send', (d) => {
+        if(users[socket.id]) io.to(users[socket.id].room).emit('msg_recv', d);
+    });
+
+    socket.on('undo', () => {
+        if(users[socket.id]) {
+            let room = users[socket.id].room;
+            let last = items[room] ? items[room].pop() : null;
+            if(last) io.to(room).emit('del_obj', last.id);
         }
     });
 
-    socket.on('disconnect', () => {
-        const user = users[socket.id];
-        if (user) {
-            const room = user.roomID;
-            delete users[socket.id];
-            io.to(room).emit('user_disconnected', socket.id);
+    socket.on('clear', () => {
+        if(users[socket.id]) {
+            items[users[socket.id].room] = [];
+            io.to(users[socket.id].room).emit('reset');
         }
     });
 });
-
-function getRoomUsers(roomID) {
-    const roomUsers = {};
-    for (const id in users) {
-        if (users[id].roomID === roomID) roomUsers[id] = users[id];
-    }
-    return roomUsers;
-}
-
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => console.log(`Server on ${PORT}`));
