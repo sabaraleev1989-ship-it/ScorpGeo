7283c2d72219d5dd93b46b523ef40e00d030778a
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-04-30 16:12:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,79 +3,45 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('public')); // Папка, где лежит твой index.html
-
-// Глобальный объект для хранения данных комнат
 const rooms = {};
 
 io.on('connection', (socket) => {
-    
-    // 1. Вход в комнату
     socket.on('join_room', (data) => {
-        const { room, name } = data;
-        socket.join(room);
-        socket.room = room;
-        socket.userName = name;
-
-        if (!rooms[room]) rooms[room] = { users: {}, objects: [] };
-        
-        // Добавляем пользователя в список
-        rooms[room].users[socket.id] = { name: name, lat: 0, lng: 0 };
-
+        socket.join(data.room);
+        socket.room = data.room;
+        socket.userName = data.name;
+        if (!rooms[data.room]) rooms[data.room] = { objects: [] };
         socket.emit('login_success');
-        
-        // Оповещаем всех: "Заходит: Позывной"
-        io.to(room).emit('player_joined', { name: name });
-        
-        // Сразу отправляем новичку текущую ситуацию в комнате
-        socket.emit('presence_update', rooms[room].users);
-        rooms[room].objects.forEach(obj => socket.emit('draw', obj));
     });
 
-    // 2. Обновление GPS позиции
-    socket.on('gps_sync', (data) => {
-        if (socket.room && rooms[socket.room]) {
-            rooms[socket.room].users[socket.id].lat = data.lat;
-            rooms[socket.room].users[socket.id].lng = data.lng;
-            
-            // Рассылаем всем обновленные координаты бойцов
-            io.to(socket.room).emit('presence_update', rooms[socket.room].users);
-        }
-    });
-
-    // 3. Создание меток (враг, цель, стрелка)
+    // Создание объекта
     socket.on('new_obj', (obj) => {
-        if (socket.room && rooms[socket.room]) {
+        if (socket.room) {
             rooms[socket.room].objects.push(obj);
             io.to(socket.room).emit('draw', obj);
         }
     });
 
-    // 4. РАДИООБМЕН (Сообщения чата)
-    socket.on('chat_msg', (msg) => {
+    // ТОЧЕЧНОЕ УДАЛЕНИЕ (Для ластика)
+    socket.on('delete_obj', (id) => {
         if (socket.room) {
-            io.to(socket.room).emit('receive_msg', msg);
+            rooms[socket.room].objects = rooms[socket.room].objects.filter(o => o.id !== id);
+            io.to(socket.room).emit('remove_obj', id);
         }
     });
 
-    // 5. Очистка карты
+    // ОЧИСТКА ВСЕГО
     socket.on('clear', () => {
-        if (socket.room && rooms[socket.room]) {
+        if (socket.room) {
             rooms[socket.room].objects = [];
             io.to(socket.room).emit('clear_all');
         }
     });
 
-    // 6. Выход игрока
-    socket.on('disconnect', () => {
-        if (socket.room && rooms[socket.room]) {
-            delete rooms[socket.room].users[socket.id];
-            io.to(socket.room).emit('presence_update', rooms[socket.room].users);
-        }
+    // ЧАТ
+    socket.on('chat_msg', (msg) => {
+        if (socket.room) io.to(socket.room).emit('receive_msg', msg);
     });
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log(`Тактический сервер запущен на порту ${PORT}`);
-});
+http.listen(3000, () => console.log('Server V16.1 Started'));
