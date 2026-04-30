0d5266fb9154080bdfdaf6d5a2f683729f26f7ce
sabaraleev1989-ship-it <sabaraleev1989@gmail.com>
2026-04-30 16:18:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,42 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
+const PORT = process.env.PORT || 3000;
+
+// Хранилище данных комнат
 const rooms = {};
 
+app.use(express.static(__dirname));
+
 io.on('connection', (socket) => {
+    // Вход в комнату
     socket.on('join_room', (data) => {
         socket.join(data.room);
         socket.room = data.room;
         socket.userName = data.name;
-        if (!rooms[data.room]) rooms[data.room] = { objects: [] };
+        
+        if (!rooms[data.room]) {
+            rooms[data.room] = { objects: [] };
+        }
+        
+        // Отправляем старые объекты новому игроку
+        rooms[data.room].objects.forEach(obj => {
+            socket.emit('draw', obj);
+        });
+        
         socket.emit('login_success');
     });
 
-    // Создание объекта
+    // Синхронизация GPS
+    socket.on('gps_sync', (data) => {
+        if (socket.room) {
+            socket.to(socket.room).emit('player_move', data);
+        }
+    });
+
+    // Создание нового объекта (точка, стрела, линейка)
     socket.on('new_obj', (obj) => {
         if (socket.room) {
             rooms[socket.room].objects.push(obj);
@@ -22,7 +45,7 @@ io.on('connection', (socket) => {
         }
     });
 
-    // ТОЧЕЧНОЕ УДАЛЕНИЕ (Для ластика)
+    // ТОЧЕЧНОЕ УДАЛЕНИЕ (Для Ластика)
     socket.on('delete_obj', (id) => {
         if (socket.room) {
             rooms[socket.room].objects = rooms[socket.room].objects.filter(o => o.id !== id);
@@ -30,7 +53,14 @@ io.on('connection', (socket) => {
         }
     });
 
-    // ОЧИСТКА ВСЕГО
+    // РАДИООБМЕН (Чат)
+    socket.on('chat_msg', (msg) => {
+        if (socket.room) {
+            io.to(socket.room).emit('receive_msg', msg);
+        }
+    });
+
+    // ПОЛНАЯ ОЧИСТКА
     socket.on('clear', () => {
         if (socket.room) {
             rooms[socket.room].objects = [];
@@ -38,10 +68,11 @@ io.on('connection', (socket) => {
         }
     });
 
-    // ЧАТ
-    socket.on('chat_msg', (msg) => {
-        if (socket.room) io.to(socket.room).emit('receive_msg', msg);
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
-http.listen(3000, () => console.log('Server V16.1 Started'));
+http.listen(PORT, () => {
+    console.log(`SCORPION SERVER V16.1 RUNNING ON PORT ${PORT}`);
+});
